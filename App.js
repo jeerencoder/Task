ffeb17751ae16937102dafa8fbe4c0bee8318ba7
ghiral <jeeren.coder@gmail.com>
2023-11-24 18:22:21 +0530

    Tast Done

--- App.js
+++ App.js
@@ -1,267 +1,254 @@
-import {
-  FlatList,
-  Image,
-  SafeAreaView,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
-} from 'react-native';
-import React, {useState} from 'react';
-import {ListData, HeaderData} from './data/dummy';
+import React, { useEffect, useState } from 'react'
+import { FlatList, SafeAreaView, StyleSheet, Text, TextInput, TouchableOpacity, View, Modal, Button } from 'react-native'
+import axios from 'axios'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
-const app = () => {
-  const [cart, setCart] = useState([]);
-  const [cartCount, setCartCount] = useState(0);
-  const [selected, setSelected] = useState(1);
+const TaskA = () => {
+  const base_url = "https://jsonplaceholder.typicode.com/posts"
+  const [data, setData] = useState([])
+  const [searchQuery, setSearchQuery] = useState("")
+  const [modalVisible, setModalVisible] = useState("")
 
-  const toggleItemCart = item => {
-    if (cart.includes(item)) {
-      const updatedCart = cart.filter(cartItem => cartItem !== item);
-      setCart(updatedCart);
-      setCartCount(cartCount - 1);
-    } else {
-      setCart([...cart, item]);
-      setCartCount(cartCount + 1);
+  const [post, setPost] = useState([])
+  const [title, setTitle] = useState("")
+  const [body, setBody] = useState("")
+
+
+  useEffect(() => {
+    fetch()
+    getData();
+  }, [])
+
+  const getData = async () => {
+    try {
+      const savedPosts = await AsyncStorage.getItem('posts');
+      if (savedPosts !== null) {
+        setPost(JSON.parse(savedPosts));
+      }
+    } catch (error) {
+      console.error('Error retrieving data:', error);
     }
-  };
+  }
 
-  const renderItem = ({item}) => (
-    <TouchableOpacity
-      style={styles.renderContainer}
-      onPress={() => {
-        setSelected(item.id);
-      }}>
-      <Image source={item.image} style={styles.image} />
-      <View
-        style={{
-          borderBottomColor: item.id === selected ? '#06D6A0' : '#fff',
-          borderBottomWidth: 1,
-          paddingVertical: 5,
-          paddingHorizontal: 10,
-        }}>
-        <Text
-          style={{
-            fontSize: 15,
-            color: item.id == selected ? '#06D6A0' : '#000',
-          }}>
-          {item.name}
-        </Text>
-      </View>
-    </TouchableOpacity>
-  );
+  const handleAdd = async () => {
+    const newPost = {
+      title: title,
+      body: body,
+      id: Date.now() + Math.random(),
+      userId: Date.now() + Math.random()
+    };
+    setPost((prevPosts) => [
+      ...prevPosts,
+      newPost
+    ]);
+    try {
+      await AsyncStorage.setItem('posts', JSON.stringify([...post, newPost]));
+    } catch (error) {
+      console.error('Error saving data:', error);
+    }
+    setTitle("");
+    setBody("");
+    setModalVisible(false)
+  }
+  const fetch = async () => {
 
-  const renderProducts = ({item}) => (
-    <View key={item.id} style={styles.VerticalContainer}>
-      <View style={styles.VContA}>
-        <Image source={item.image} style={styles.renderImage} />
+    try {
+      const res = await axios.get(base_url)
+      console.log(res.data, "call")
+      setData([...post, ...res.data]);
+
+    } catch (error) {
+    }
+  }
+
+  const searchedData = (data, searchQuery) => {
+    return data.filter((item) => {
+      const title = item.title.toLowerCase().includes(searchQuery.toLowerCase());
+      const body = item.body.toLowerCase().includes(searchQuery.toLowerCase());
+      return title && body;
+    });
+  }
+
+  const filteredData = searchedData(data, searchQuery);
+
+  const renderItem = ({ item }) => {
+    return (
+      <View style={styles.item}>
+        <Text style={styles.title}>{item.title}</Text>
+        <Text style={styles.dec}>{item.body}</Text>
       </View>
-      <View style={styles.VContB}>
-        <Text style={styles.name}>{item?.name}</Text>
-        {item?.category === null ? null : (
-          <Text style={styles.category}>{item?.category}</Text>
-        )}
-        {item?.description == null ? null : (
-          <Text style={styles.description}>{item?.description}</Text>
-        )}
-        <Text style={styles.Price}>${item?.Price}</Text>
-        <Text style={styles.quantity}>{item?.quantity}</Text>
-      </View>
-      <TouchableOpacity
-        onPress={() => toggleItemCart(item.id)}
-        style={styles.VContC}>
-        <View style={styles.AddButton}>
-          <Image
-            source={require('./src/Assets/icons/add.png')}
-            style={styles.AddIcon}
-          />
-          <Text style={styles.Button}>{'Add'}</Text>
-        </View>
-      </TouchableOpacity>
-    </View>
-  );
+    )
+  }
 
   return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.NavBar}>
-        <View style={styles.backView}>
-          <TouchableOpacity onPress={() => console.log('goback')}>
-            <Image
-              source={require('./src/Assets/icons/main-back.png')}
-              style={styles.backIcon}
-              resizeMode="contain"
-            />
-          </TouchableOpacity>
-          <Text style={styles.backTitle}>Products</Text>
+    <SafeAreaView style={styles.safeAreaView}>
+      <View style={styles.contain}>
+        <View>
+          <TextInput
+            style={styles.input}
+            placeholder="Search..."
+            onChangeText={(text) => setSearchQuery(text)}
+            value={searchQuery}
+          />
         </View>
+        <View style={styles.addButton}>
+          <TouchableOpacity
+            onPress={() => { setModalVisible(true) }}>
 
-        <View style={styles.iconCont}>
-          <Image
-            source={require('./src/Assets/icons/search.png')}
-            style={styles.searchIcon}
-          />
-
-          <Image
-            source={require('./src/Assets/icons/Group.png')}
-            style={styles.cartIcon}
-          />
-          {cartCount === 0 ? null : (
-            <View style={styles.cartBadge}>
-              <Text style={styles.cartCount}>{cartCount}</Text>
-            </View>
-          )}
+            <Text style={styles.addText}>Add New Post</Text>
+          </TouchableOpacity>
         </View>
       </View>
-
-      <View style={styles.ListA}>
+      <View style={styles.container}>
         <FlatList
-          showsHorizontalScrollIndicator={false}
-          data={HeaderData}
-          horizontal
+          data={filteredData}
           renderItem={renderItem}
           keyExtractor={item => item.id}
         />
       </View>
-
-      <View style={styles.ListB}>
-        <FlatList
-          data={ListData}
-          renderItem={renderProducts}
-          keyExtractor={item => item.id}
-        />
-      </View>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          Alert.alert('Modal has been closed.');
+          setModalVisible(!modalVisible);
+        }}>
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>add your post</Text>
+            <TextInput
+              style={styles.inputqq}
+              placeholder="Enter your post here"
+              multiline
+              value={title}
+              onChangeText={(text) => setTitle(text)}
+            />
+            <TextInput
+              style={styles.inputqq}
+              placeholder="Enter your post here"
+              multiline
+              value={body}
+              onChangeText={(text) => setBody(text)}
+            />
+            <Button title={'Add Post'} onPress={handleAdd} />
+          </View>
+        </View>
+      </Modal>
     </SafeAreaView>
-  );
-};
 
-export default app;
+  )
+}
+
+export default TaskA
 
 const styles = StyleSheet.create({
+  safeAreaView: {
+    flex: 1,
+  },
   container: {
     flex: 1,
   },
   item: {
-    padding: 20,
+    backgroundColor: '#fff',
+    borderRadius: 10,
+    padding: 5,
+    marginVertical: 5,
+    marginHorizontal: 15,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
   },
   title: {
-    fontSize: 32,
+    fontSize: 14,
+    paddingBottom: 10,
   },
-  renderContainer: {
+  dec: {
+    fontSize: 12,
+  },
+  input: {
+    backgroundColor: '#fff',
+    width: 200,
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    borderColor: "#c2c2c2",
+    padding: 10,
+    padding: 5,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  centeredView: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    marginHorizontal: 10,
+    marginTop: 22,
   },
-  image: {
-    height: 60,
-    width: 60,
-    borderRadius: 50,
-  },
-  title: {
-    fontSize: 15,
-    color: '#06D6A0',
-  },
-  VerticalContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    borderBottomColor: '#c2c2c2',
-    borderBottomWidth: 1,
-    marginHorizontal: 20,
-    marginVertical: 10,
-    paddingBottom: 10,
-  },
-  Button: {
-    color: '#06D6A0',
-    fontSize: 14,
-  },
-  name: {
-    fontSize: 15,
-    color: '#000',
-  },
-  category: {
-    fontSize: 14,
-    color: '#000',
-  },
-  description: {
-    fontSize: 14,
-    color: '#c2c2c2',
-  },
-  Price: {
-    fontSize: 20,
-    color: '#000',
-  },
-  quantity: {
-    fontSize: 15,
-    color: '#c2c2c2',
-  },
-  AddButton: {
-    flexDirection: 'row',
-    alignItems: 'center',
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
     borderRadius: 20,
+    padding: 45,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  buttonOpen: {
+    backgroundColor: '#F194FF',
+  },
+  buttonClose: {
+    backgroundColor: '#2196F3',
+  },
+  textStyle: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: 'center',
+  },
+  inputqq: {
+    backgroundColor: '#fff',
+    width: 170,
+    height: 40,
+    margin: 12,
     borderWidth: 1,
-    borderColor: '#c2c2c238',
-    padding: 1,
-    justifyContent: 'center',
+    borderColor: "#c2c2c2",
+    padding: 10,
+    padding: 5,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
   },
-  NavBar: {
-    flexDirection: 'row',
-    width: '100%',
-    height: 50,
-    paddingHorizontal: 10,
-    justifyContent: 'space-between',
-    borderBottomWidth: 1,
-    borderBottomColor: '#000',
-  },
-  searchIcon: {
-    width: 35,
-    height: 35,
-    paddingRight: 1,
-    resizeMode: 'contain',
-  },
-  cartIcon: {
-    width: 35,
-    height: 35,
-    marginHorizontal: 10,
-    position: 'relative',
-    resizeMode: 'contain',
-  },
-  cartBadge: {
-    position: 'absolute',
-    top: 1,
-    right: 1,
-    backgroundColor: 'red',
-    borderRadius: 50,
-    width: 20,
-    height: 20,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  VContA: {
-    width: '25%',
-  },
-  VContB: {
-    width: '60%',
-    paddingLeft: 10,
-  },
-  VContC: {
-    alignSelf: 'flex-start',
-    width: '15%',
-  },
-  AddIcon: {
-    width: 10,
-    height: 10,
-    paddingRight: 1,
-  },
-  renderImage: {width: 90, height: 90},
-  cartCount: {fontSize: 15, color: '#fff'},
-  backTitle: {fontSize: 25, marginLeft: 15, color: '#000'},
-  iconCont: {
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-  backIcon: {width: 30, height: 25},
-  backView: {flexDirection: 'row', alignItems: 'center'},
-  ListA: {marginTop: 15},
-  ListB: {marginBottom: 150},
-});
+  contain: { flexDirection: "row", justifyContent: "space-around" },
+  addButton: { borderRadius: 10, backgroundColor: "green", justifyContent: "center", alignItems: "center", marginVertical: 10, paddingHorizontal: 10 },
+  addText: { color: "white" }
+})
---
+++ main/task1/TaskA.js
@@ -0,0 +1,272 @@
+import { FlatList, SafeAreaView, StyleSheet, Text, TextInput, TouchableOpacity, View, Modal, Button } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+
+const TaskA = () => {
+    const base_url = "https://jsonplaceholder.typicode.com/posts"
+    const [data, setData] = useState([])
+    const [searchQuery, setSearchQuery] = useState("")
+    const [modalVisible, setModalVisible] = useState("")
+
+    // 
+    const [post, setPost] = useState([])
+    const [title, setTitle] = useState("")
+    const [body, setBody] = useState("")
+
+    useEffect(() => {
+        const getData = async () => {
+            try {
+                const savedPosts = await AsyncStorage.getItem('posts');
+                if (savedPosts !== null) {
+                    setPost(JSON.parse(savedPosts));
+                }
+            } catch (error) {
+                console.error('Error retrieving data:', error);
+            }
+        };
+        getData();
+    }, []);
+
+    const handleAdd = async () => {
+        const newPost = {
+            title: title,
+            body: body,
+            id: Date.now() + Math.random(),
+            userId: Date.now() + Math.random()
+        };
+        setPost((prevPosts) => [
+            ...prevPosts,
+            newPost
+        ]);
+        try {
+            await AsyncStorage.setItem('posts', JSON.stringify([...post, newPost]));
+        } catch (error) {
+            console.error('Error saving data:', error);
+        }
+        setTitle("");
+        setBody("");
+        setModalVisible(false)
+    }
+
+    console.log(post, "post")
+
+
+    const handleDelete = async (postId) => {
+        try {
+            const updatedPosts = post.filter((p) => p.id !== postId);
+            await AsyncStorage.setItem('posts', JSON.stringify(updatedPosts));
+            setPost(updatedPosts);
+        } catch (error) {
+            console.error('Error deleting data:', error);
+        }
+    };
+    // 
+    const fetch = async () => {
+
+        try {
+            const res = await axios.get(base_url)
+            console.log(res.data, "call")
+            setData([...post, ...res.data]);
+
+        } catch (error) {
+        }
+    }
+
+    useEffect(() => {
+        fetch()
+    }, [])
+
+
+    const searchedData = (data, searchQuery) => {
+        return data.filter((item) => {
+            const title = item.title.toLowerCase().includes(searchQuery.toLowerCase());
+            const body = item.body.toLowerCase().includes(searchQuery.toLowerCase());
+            return title && body;
+        });
+    }
+
+    const filteredData = searchedData(data, searchQuery);
+
+    const renderItem = ({ item }) => {
+        return (
+
+            <View style={styles.item}>
+                <Text style={styles.title}>{item.title}</Text>
+                <Text style={styles.dec}>{item.body}</Text>
+            </View>
+        )
+    }
+
+    return (
+        <SafeAreaView style={styles.safeAreaView}>
+            <View style={{ flexDirection: "row", justifyContent: "space-around", }}>
+                <View>
+                    <TextInput
+                        style={styles.input}
+                        placeholder="Search..."
+                        onChangeText={(text) => setSearchQuery(text)}
+                        value={searchQuery}
+                    />
+                </View>
+                <View style={{ borderRadius: 10, backgroundColor: "green", justifyContent: "center", alignItems: "center", marginVertical: 10, paddingHorizontal: 10 }}>
+                    <TouchableOpacity
+                        onPress={() => { setModalVisible(true) }}>
+
+                        <Text>Add New Post</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+            <View style={styles.container}>
+                <FlatList
+                    data={filteredData}
+                    renderItem={renderItem}
+                    keyExtractor={item => item.id}
+                    ListFooterComponent={() => {
+                        return <View style={{ marginBottom: 50, }}></View>
+                    }}
+                />
+            </View>
+            <Modal
+                animationType="slide"
+                transparent={true}
+                visible={modalVisible}
+                onRequestClose={() => {
+                    Alert.alert('Modal has been closed.');
+                    setModalVisible(!modalVisible);
+                }}>
+                <View style={styles.centeredView}>
+                    <View style={styles.modalView}>
+                        <Text style={styles.modalText}>add your post</Text>
+                        <TextInput
+                            style={styles.inputqq}
+                            placeholder="Enter your post here"
+                            multiline
+                            value={title}
+                            onChangeText={(text) => setTitle(text)}
+                        />
+                        <TextInput
+                            style={styles.inputqq}
+                            placeholder="Enter your post here"
+                            multiline
+                            value={body}
+                            onChangeText={(text) => setBody(text)}
+                        />
+                        <Button title={'Add Post'} onPress={handleAdd} />
+                    </View>
+                </View>
+            </Modal>
+        </SafeAreaView>
+
+    )
+}
+
+export default TaskA
+
+const styles = StyleSheet.create({
+    safeAreaView: {
+        flex: 1,
+    },
+    container: {
+        flex: 1,
+    },
+    item: {
+        backgroundColor: '#fff',
+        borderRadius: 10,
+        padding: 5,
+        marginVertical: 5,
+        marginHorizontal: 15,
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 5,
+    },
+    title: {
+        fontSize: 14,
+        paddingBottom: 10,
+    },
+    dec: {
+        fontSize: 12,
+    },
+    input: {
+        backgroundColor: '#fff',
+        width: 200,
+        height: 40,
+        margin: 12,
+        borderWidth: 1,
+        borderColor: "#c2c2c2",
+        padding: 10,
+        padding: 5,
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 5,
+    },
+    centeredView: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 22,
+    },
+    modalView: {
+        margin: 20,
+        backgroundColor: 'white',
+        borderRadius: 20,
+        padding: 45,
+        alignItems: 'center',
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5,
+    },
+    button: {
+        borderRadius: 20,
+        padding: 10,
+        elevation: 2,
+    },
+    buttonOpen: {
+        backgroundColor: '#F194FF',
+    },
+    buttonClose: {
+        backgroundColor: '#2196F3',
+    },
+    textStyle: {
+        color: 'white',
+        fontWeight: 'bold',
+        textAlign: 'center',
+    },
+    modalText: {
+        marginBottom: 15,
+        textAlign: 'center',
+    },
+    inputqq: {
+        backgroundColor: '#fff',
+        width: 170,
+        height: 40,
+        margin: 12,
+        borderWidth: 1,
+        borderColor: "#c2c2c2",
+        padding: 10,
+        padding: 5,
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 5,
+    },
+})
